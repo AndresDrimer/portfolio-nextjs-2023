97b78bc49d471012163ca5f64c266c0539cc92e5
AndresDrimer <andresdrimer@hotmail.com>
2023-03-28 08:55:30 -0300

    tenzies added, paths corrected

--- data.js
+++ data.js
@@ -1,171 +1,169 @@
 export default {
-        name: "Andrés Drimer",
-        workTitle: "front-end Web Developer",
-        moto: "I love to build and design responsive websites to connect people with people.",
-        logo:"/assets/logoAD2023-4.png",
-        sections: ["home", "about", "skills", "projects", "contact"],
-        aboutPhoto: "/assets/portrait2.jpeg",
-        contact: [
-            {
-                linkedin: "",
-                github:"",
-                email:"",
-                whatsapp:"",
-            }
-        ],
-        resume:"",
-        about: {
-            subtitle:"Everybody´s path is unique. This is mine...",
-            paragraph1:"I started to work in Cafe de las Ciudades, a book publisher specialized in Urbanism, in 2019. I was in charge of comercialization, a role which included dealing with clients, suppliers and book shops. I suggested to open an ecommerce site, which I created with a CMS (TiendaNube). I also ran a strong ad-campaign in social media. They were thrilled to see the result (and their facturation growing up to 7 times!), so then I was in charge of re-designing the website for their monthly newsletter, in its 20th anniversary. I managed to do that with another CMS (Wordpress). I got more and more deeply involved in programing websites and suddenly I realized I that had encountered my new passion. I´ve got really focused on learning everything about web developing while working in a lot of different projects for people around (and later for unkowned clients in Feverr). The word kept running mouth to mouth and I ended building and designing a lot of projects to reach different expectations. Now I have a strong desire to be involved in a group of people to participate in building bigger things, keep the level of complexity going up to continue learning and growing as developer. ",
-            paragraph2:"I think I can add a really good base on soft skills, I´m used to work with people from different cultures and can comunicate clearly in English, Spanish and Portuguese. I´ve work in the music industry previously, in the production field, so I´m used to deal with deadlines, group leadership and tricky commercial affaires. "
-        },
-        skills: [ 
-            {
-                id:0,
-                tech:"next js",
-                version:"13",
-                icon:"/assets/skills/nextjs.png"
-            },
-            {
-                id:1,
-                tech:"react",
-                version:"18",
-                icon:"/assets/skills/react.png"
-            },
-            {
-                id:2,
-                tech:"javascript",
-                version: "ECMAScript 2020",
-                icon:"/assets/skills/javascript.png"
-            },
-            {
-                id:3,
-                tech:"html",
-                version:"5",
-                icon:"/assets/skills/html.png"
-            },
-            {
-                id:4,
-                tech:"css",
-                version:"3",
-                icon:"/assets/skills/css.png"
-            },
-            {
-                id:5,
-                tech:"tailwind css",
-                version:"3",
-                icon:"/assets/skills/tailwind.png"
-            },
-            {
-                id:6,
-                tech:"bootstrap",
-                version:"5",
-                icon:"/assets/skills/bootstrap.png"
-            },
-            {
-                id:7,
-                tech:"git",
-                icon:"/assets/skills/git.png"
-            },
-            {
-                id:8,
-                tech:"github",
-                icon:"/assets/skills/github.png"
-            },   
-            {
-                id:9,
-                tech:"firebase",
-                icon:"/assets/skills/firebase.png"
-            },
-            {
-                id:10,
-                tech:"tiendanube (CMS)",
-                icon:"/assets/skills/tiendanube.png"
-            },
-            {
-                id:11,
-                tech:"figma",
-                icon:"/assets/skills/figma.png"
-            },
-            {
-                id:12,
-                tech:"photoshop",
-                icon:"/assets/skills/photoshop.png"
-            },
-            {
-                id:13,
-                tech:"premiere",
-                icon:"/assets/skills/premiere.png"
-            },
-        ],
-        projects:[
-            {
-                id:0,
-                imageBck:"/assets/projects/bitcoin/bitcoin-img-0.png",
-                title:"Bitcoin´s observer",
-                tech:"HTML/CSS/JAVASCRIPT",
-                url:""
-            },
-            {
-                id:1,
-                imageBck:"/assets/projects/calculator/calc-img-0.png",
-                title:"Simple calculator",
-                tech:"HTML/CSS/JAVASCRIPT",
-                url:""
-            },
-            {
-                id:1,
-                imageBck:"/assets/projects/cdlc/cdlc-img-0.png",
-                title:"Blog & E-commerce for Urbanism Publisher",
-                tech:"CMS (Tiendanube, Wordpress) - FIGMA, Photoshop",
-                url:""
-            },
-            {
-                id:2,
-                imageBck:"/assets/projects/garage/garage-img-0.png",
-                title:"Store",
-                tech:"Bootstrap",
-                url:""
-            },
-            {
-                id:3,
-                imageBck:"/assets/projects/harry/harry-img-0.png",
-                title:"Harry Potter Trivia",
-                tech:"HTML/CSS/JAVASCRIPT",
-                url:""
-            },
-            {
-                id:4,
-                imageBck:"/assets/projects/max/max-img-1.png",
-                title:"Website & E-commerce for artist (Max Aguirre)",
-                tech:"HTML/CSS/JAVASCRIPT - CMS (Tiendanube)",
-                url:""
-            },
-            {
-                id:5,
-                imageBck:"/assets/projects/pablo/pablo-website-landscpae.png",
-                title:"Website for videomaker (Pablo coronel)",
-                tech:"REACT",
-                url:""
-            },
-            {
-                id:6,
-                imageBck:"/assets/projects/ppt/ppt-img-0.png",
-                title:"Paper, Rock & Scissors game",
-                tech:"HTML/CSS/JAVASCRIPT",
-                url:""
-            },
-            {
-                id:7,
-                imageBck:"",
-                title:"Tenzies game",
-                tech:"REACT",
-                url:""
-            },
+  name: "Andrés Drimer",
+  workTitle: "front-end Web Developer",
+  moto: "I love to build and design responsive websites to connect people with people.",
+  logo: "/assets/logoAD2023-4.png",
+  sections: ["home", "about", "skills", "projects", "contact"],
+  aboutPhoto: "/assets/portrait2.jpeg",
+  contact: [
+    {
+      linkedin: "",
+      github: "",
+      email: "",
+      whatsapp: "",
+    },
+  ],
+  resume: "",
+  about: {
+    subtitle: "Everybody´s path is unique. This is mine...",
+    paragraph1:
+      "I started to work in Cafe de las Ciudades, a book publisher specialized in Urbanism, in 2019. I was in charge of comercialization, a role which included dealing with clients, suppliers and book shops. I suggested to open an ecommerce site, which I created with a CMS (TiendaNube). I also ran a strong ad-campaign in social media. They were thrilled to see the result (and their facturation growing up to 7 times!), so then I was in charge of re-designing the website for their monthly newsletter, in its 20th anniversary. I managed to do that with another CMS (Wordpress). I got more and more deeply involved in programing websites and suddenly I realized I that had encountered my new passion. I´ve got really focused on learning everything about web developing while working in a lot of different projects for people around (and later for unkowned clients in Feverr). The word kept running mouth to mouth and I ended building and designing a lot of projects to reach different expectations. Now I have a strong desire to be involved in a group of people to participate in building bigger things, keep the level of complexity going up to continue learning and growing as developer. ",
+    paragraph2:
+      "I think I can add a really good base on soft skills, I´m used to work with people from different cultures and can comunicate clearly in English, Spanish and Portuguese. I´ve work in the music industry previously, in the production field, so I´m used to deal with deadlines, group leadership and tricky commercial affaires. ",
+  },
+  skills: [
+    {
+      id: 0,
+      tech: "next js",
+      version: "13",
+      icon: "/assets/skills/nextjs.png",
+    },
+    {
+      id: 1,
+      tech: "react",
+      version: "18",
+      icon: "/assets/skills/react.png",
+    },
+    {
+      id: 2,
+      tech: "javascript",
+      version: "ECMAScript 2020",
+      icon: "/assets/skills/javascript.png",
+    },
+    {
+      id: 3,
+      tech: "html",
+      version: "5",
+      icon: "/assets/skills/html.png",
+    },
+    {
+      id: 4,
+      tech: "css",
+      version: "3",
+      icon: "/assets/skills/css.png",
+    },
+    {
+      id: 5,
+      tech: "tailwind css",
+      version: "3",
+      icon: "/assets/skills/tailwind.png",
+    },
+    {
+      id: 6,
+      tech: "bootstrap",
+      version: "5",
+      icon: "/assets/skills/bootstrap.png",
+    },
+    {
+      id: 7,
+      tech: "git",
+      icon: "/assets/skills/git.png",
+    },
+    {
+      id: 8,
+      tech: "github",
+      icon: "/assets/skills/github.png",
+    },
+    {
+      id: 9,
+      tech: "firebase",
+      icon: "/assets/skills/firebase.png",
+    },
+    {
+      id: 10,
+      tech: "tiendanube (CMS)",
+      icon: "/assets/skills/tiendanube.png",
+    },
+    {
+      id: 11,
+      tech: "figma",
+      icon: "/assets/skills/figma.png",
+    },
+    {
+      id: 12,
+      tech: "photoshop",
+      icon: "/assets/skills/photoshop.png",
+    },
+    {
+      id: 13,
+      tech: "premiere",
+      icon: "/assets/skills/premiere.png",
+    },
+  ],
+  projects: [
+    {
+      id: 0,
+      imageBck: "/assets/projects/pablo/pablo-website-landscape.png",
+      title: "Website for videomaker (Pablo coronel)",
+      tech: "REACT",
+      url: "",
+    },
+    {
+      id: 1,
+      imageBck: "/assets/projects/tenzies/tenzies.png",
+      title: "Tenzies game",
+      tech: "REACT",
+      url: "https://tenziesgameforcataandpabli.netlify.app/",
+    },
+    {
+      id: 2,
+      imageBck: "/assets/projects/max/max-img-1.png",
+      title: "Website & E-commerce for artist (Max Aguirre)",
+      tech: "HTML/CSS/JAVASCRIPT - CMS (Tiendanube)",
+      url: "",
+    },
+    {
+      id: 3,
+      imageBck: "/assets/projects/cdlc/cdlc-img-0.png",
+      title: "Blog & E-commerce for Urbanism Publisher",
+      tech: "CMS (Tiendanube, Wordpress) - FIGMA",
+      url: "",
+    },
+    {
+      id: 4,
+      imageBck: "/assets/projects/bitcoin/bitcoin-img-0.png",
+      title: "Bitcoin´s observer",
+      tech: "HTML/CSS/JAVASCRIPT",
+      url: "",
+    },
+    {
+      id: 5,
+      imageBck: "/assets/projects/garage/garage-img-0.png",
+      title: "Store",
+      tech: "Bootstrap",
+      url: "",
+    },
+    {
+      id: 6,
+      imageBck: "/assets/projects/ppt/ppt-img-0.png",
+      title: "Paper, Rock & Scissors game",
+      tech: "HTML/CSS/JAVASCRIPT",
+      url: "",
+    },
+    {
+      id: 7,
+      imageBck: "/assets/projects/calculator/calc-img-0.png",
+      title: "Simple calculator",
+      tech: "HTML/CSS/JAVASCRIPT",
+      url: "",
+    },
 
-            
-
-    ]
-            
-
-    }
+    {
+      id: 8,
+      imageBck: "/assets/projects/harry/harry-img-0.png",
+      title: "Harry Potter Trivia",
+      tech: "HTML/CSS/JAVASCRIPT",
+      url: "",
+    },
+  ],
+};
