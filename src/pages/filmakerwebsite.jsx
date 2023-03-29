3597b4e7dbc42b1c76222acc28ef7c74d03d9379
AndresDrimer <andresdrimer@hotmail.com>
2023-03-29 17:07:52 -0300

    more media queries project

--- src/pages/filmakerwebsite.jsx
+++ src/pages/filmakerwebsite.jsx
@@ -15,8 +15,8 @@ export default function filmakerwebsite() {
   
   return (
     <div className="w-full">
-      <div className="w-screen relative h-[15vh] lg:h-[20vh] ">
-        <div className="absolute top-0 left-0 w-full h-[15vh] lg:h-[20vh] bg-black/80 z-10" />
+      <div className="w-screen relative h-[20vh] lg:h-[30vh] ">
+        <div className="absolute top-0 left-0 w-full h-[20vh] lg:h-[30vh] bg-black/80 z-10" />
         
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
           <h2 className="py-2">{data.projects[0].title}</h2>
